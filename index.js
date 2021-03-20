0152261743fccc480b73ce4522c55ed7aa89dd10
Dipu <dipukumarmohanto1@gmail.com>
2021-03-20 11:12:12 +0600

    Event Handler to onClick event

--- index.js
+++ index.js
@@ -1,21 +1,9 @@
-/* 
-            querySelector()
-            querySelectorAll()
+var toChange = document.querySelector("img");
 
-Usage:
-            tag-name
-            #id-name
-            .class-name
-*/
-/* 
-document.querySelector("#pid").innerHTML = "Changed";
+function alertMessage1() {
+    toChange.src = "assets/sea-bird-1.jpg";
+}
 
-document.querySelector(".heading").innerHTML = "Query Selector used"; // return first heading class
-
-document.querySelectorAll(".heading")[0].innerHTML = "querySelectorAll(0)";
-document.querySelectorAll(".heading")[1].innerHTML = "querySelectorAll(1)";
-document.querySelectorAll(".heading")[2].innerHTML = "querySelectorAll(2)"; */
-
-document.querySelector("a").innerHTML = "Changed Home";
-document.querySelector("li a").innerHTML = "Changed Tutorial";
-document.querySelector(".my-div a").innerHTML = "Changed Contact";
+function alertMessage2() {
+    toChange.src = "assets/sea-bird-2.jpg";
+}
