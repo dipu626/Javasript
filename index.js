2ce7af03d1ff62d039609d588a19349a1b226ecd
Dipu <dipukumarmohanto1@gmail.com>
2021-03-20 13:23:01 +0600

    More on DOM - creating, adding, removing elements

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-var toChange = document.querySelector("img");
+// Create Eliment
 
-function alertMessage1() {
-    toChange.src = "assets/sea-bird-1.jpg";
-}
+var createEliment = document.createElement("h3");
+var newText = document.createTextNode("This is H3 heading");
+createEliment.appendChild(newText);
 
-function alertMessage2() {
-    toChange.src = "assets/sea-bird-2.jpg";
-}
+var myDiv = document.getElementById("container");
+myDiv.appendChild(createEliment);
+
+var heading2 = document.getElementsByTagName("h2")[0];
+myDiv.removeChild(heading2);
+
+var createEliment2 = document.createElement("p");
+var text = document.createTextNode("This is paragraph");
+createEliment2.appendChild(text);
+
+// Insert in a particular index
+var heading3 = document.getElementsByTagName("h3")[0];
+myDiv.insertBefore(createEliment2, heading3);
