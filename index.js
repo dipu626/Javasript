cdcc10a9041cd8c705b9eca526100ccf2f8b37e6
Dipu <dipukumarmohanto1@gmail.com>
2021-03-20 09:56:17 +0600

    Introduction to DOM - select html elements

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-function onClick() {
-    alert("Welcome");
-    console.log('Button Clicked');
-    document.write("message write using document object");
-}
+/* // getElementById
+
+var myHeading = document.getElementById("heading-1");
+myHeading.innerHTML = "Hello";
+
+var myHeading2 = document.getElementById("heading-2");
+myHeading2.innerHTML = "Hello Boy";
+
+var paragrap1 = document.getElementById("p-1");
+paragrap1.innerHTML = "lorem";
+ */
+
+/* // getElementsByTagName
+
+var headings = document.getElementsByTagName("h1");
+headings[0].innerHTML = "heading 0";
+headings[1].innerHTML = "heading 1"; */
+
+// getElementsByClassName
+var headingsByClassName = document.getElementsByClassName("heading");
+headingsByClassName[0].innerHTML = "Class 0";
+headingsByClassName[1].innerHTML = "Class 1";
