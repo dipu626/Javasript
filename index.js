29a0c1747d6ac7da0d61a809a5418221e6abecd9
Dipu <dipukumarmohanto1@gmail.com>
2021-03-21 16:56:03 +0600

    Keypress, Keydown, Keyup listener

--- index.js
+++ index.js
@@ -1,21 +1,14 @@
-let buttons = document.querySelectorAll("button");
-
-for (var i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener("click", function(){
-        var text = this.innerHTML;
-        var imgtag = document.querySelector("img");
-        var link = "assets/" + text + ".jpg";
-        imgtag.src = link;
-        imgtag.alt = "photos";
-        console.log(link);
-        addAnimation(text);
-    });
-}
-
-function addAnimation(text) {
-    var btn = document.querySelector("." + text);
-    btn.classList.add("anim");
-    setTimeout(function() {
-        btn.classList.remove("anim");
-    }, 2000);
-}
+var letters = 0;
+var textarea = document.querySelector("textarea");
+textarea.addEventListener("keydown", function(event) {
+    const key = event.key;
+    console.log(key);
+    if (key === "Backspace") {
+        letters = Math.max(0, --letters);
+    }
+    else {
+        ++letters;
+    }
+    var ptag = document.querySelector("p");
+    ptag.innerHTML = "Total letters = " + letters;
+});
