a041c9ee2a3e9c933f21d416325aee5ff72a1dbe
Dipu <dipukumarmohanto1@gmail.com>
2021-03-21 17:11:40 +0600

    try catch finally

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-var letters = 0;
-var textarea = document.querySelector("textarea");
-textarea.addEventListener("keydown", function(event) {
-    const key = event.key;
-    console.log(key);
-    if (key === "Backspace") {
-        letters = Math.max(0, --letters);
-    }
-    else {
-        ++letters;
-    }
-    var ptag = document.querySelector("p");
-    ptag.innerHTML = "Total letters = " + letters;
-});
+try {
+    // code test
+    alert("Hi Everybody");
+    alert(x);
+
+} catch (error) {
+    // handle error
+    console.log(error.name + " " + error.message);
+    console.log(error);    
+} finally {
+    alert("Goodbye Everybody");
+}
