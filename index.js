834e049efc0081ab85411bb3524ca244f6d2b70f
Dipu <dipukumarmohanto1@gmail.com>
2021-03-21 12:54:16 +0600

    Add Audio file

--- index.js
+++ index.js
@@ -1,30 +1,12 @@
-
-var photos = ["assets/sea-bird-1.jpg", "assets/sea-bird-2.jpg", "assets/photo-1.jpg"];
-var photosName = ["sea-bird 1", "sea-bird 2", "sea -bird 3"];
-
 var totalButton = document.querySelectorAll("button").length;
+let allButton = document.querySelectorAll("button");
 
+var music = ["audio/complete.mp3", "audio/begin.mp3"];
 
 for (var i = 0; i < totalButton; i++) {
-    document.querySelectorAll("button")[i].addEventListener("click", function() {
-        var text = this.innerHTML;
-        document.querySelector("h1").innerHTML = text;
-
-        switch(text) {
-            case "Button - 1":
-                document.querySelector("img").src = photos[0];
-                document.querySelector("img").alt = photosName[0];
-                break;
-            case "Button - 2":
-                document.querySelector("img").src = photos[1];
-                document.querySelector("img").alt = photosName[1];
-                break;
-            case "Button - 3":
-                document.querySelector("img").src = photos[2];
-                document.querySelector("img").alt = photosName[2];
-                break;
-            case "default":
-                break;
-        }
+    allButton[i].addEventListener("click", function() {
+        var link = "audio/" + this.innerHTML + ".mp3";
+        var audio = new Audio(link);
+        audio.play();
     });
 }
