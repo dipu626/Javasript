2309995a6e94bc74a6aee3d08a8c27b4d6063d76
Dipu <dipukumarmohanto1@gmail.com>
2021-03-22 09:25:37 +0600

    Error handling - throw keyword

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-try {
-    // code test
-    alert("Hi Everybody");
-    alert(x);
+// Error handling - try, catch, finally
+// The throw statement - create custom errors.
 
-} catch (error) {
-    // handle error
-    console.log(error.name + " " + error.message);
-    console.log(error);    
-} finally {
-    alert("Goodbye Everybody");
-}
+var sumButton = document.querySelector("#sumButton");
+sumButton.addEventListener("click", function() {
+    var num1 = document.querySelector("#input-1").value;
+    var num2 = document.querySelector("#input-2").value;
+    try {
+        console.log("num1=" + num1 + " num2=" + num2);
+        if (num1 < 0 || num2 < 0) {
+            throw "Input is too low";
+        }
+        else if (num1 > 100 || num2 > 0) {
+            throw "Input is too high";
+        }
+    } catch (error) {
+        console.log(error);
+    } 
+    var sum = parseInt(num1) + parseInt(num2);
+    document.querySelector("#displaySum").innerHTML = sum;
+});
