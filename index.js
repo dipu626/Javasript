5292a433768c9cc02388dd8b123d3a0686e6ae32
Dipu <dipukumarmohanto1@gmail.com>
2021-03-22 10:40:31 +0600

    ES6 - Hoisting & Strict mode

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
-// Variable declaration
+/*
+ES6 Features
+        1. ES6 Syntax
+            1.1 ES6 variables -> var, let, const (scope, redeclaration, value assigning)
+            1.2 Template Literals
+            1.3 Hoisting & strict mode
+            1.4 Default and rest parameter
+            1.5 spread operator
+            1.6 for...of
+        2. Arrow functions
+        3. Destructuring
+            3.1 Array and Object Destructuring
+*/
 
-/* let x = 10;
+// use strict -> variable declare na kore use kora jabe na
+"use strict"
+
+x = 20;
+console.log(`x=${x}`);
 if (true) {
-    let x = 9;
-    console.log("if=" + x);
+    y = 10;
+    console.log(`y = ${y}`);
+    var y;
 }
-console.log(x);
- */
-
-/* let x = 10;
-let y = 20;
-let sum = x + y;
-console.log(`sum = ${sum}. Thank you.`);
-
-let name = "Dipu";
-let message = "Welcome";
-console.log(`${message} to javascript ${name}`);
- */
-const add = (x, y) => {
-    console.log(`sum = ${x + y}`);
-}
-add(40, 12);
