691db9a282ae871c11dda632ae2c25978a6e902a
Dipu <dipukumarmohanto1@gmail.com>
2021-03-22 10:07:20 +0600

    Canvas - drawing graphics on web page

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-// Error handling - try, catch, finally
-// The throw statement - create custom errors.
 
-var sumButton = document.querySelector("#sumButton");
-sumButton.addEventListener("click", function() {
-    var num1 = document.querySelector("#input-1").value;
-    var num2 = document.querySelector("#input-2").value;
-    try {
-        console.log("num1=" + num1 + " num2=" + num2);
-        if (num1 < 0 || num2 < 0) {
-            throw "Input is too low";
-        }
-        else if (num1 > 100 || num2 > 0) {
-            throw "Input is too high";
-        }
-    } catch (error) {
-        console.log(error);
-    } 
-    var sum = parseInt(num1) + parseInt(num2);
-    document.querySelector("#displaySum").innerHTML = sum;
-});
+var canvasTag = document.getElementById("canvas-1");
+var ctx = canvasTag.getContext("2d");
+
+ctx.lineWidth = "3";
+ctx.strokeStyle = "purple";
+ctx.strokeRect(10, 10, 380, 280);
+
+ctx.fillStyle = "green";
+ctx.fillRect(13, 13, 374, 274);
+
+var centerX = canvasTag.width / 2;
+var centerY = canvasTag.height / 2;
+
+ctx.beginPath();
+ctx.arc(centerX, centerY, 80, 0, 2 * Math.PI, false);
+ctx.fillStyle = "red";
+ctx.fill();
+ctx.lineWidth = "1";
+ctx.stroke();
