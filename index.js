d5a13b076c495fb4c25a52fc4ceed25fcc02f700
Dipu <dipukumarmohanto1@gmail.com>
2021-03-22 14:26:55 +0600

    ES6 - Arrow function

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
-// forEach, map, filter
+// Arrow function 
 
-let numbers = [2, 3, 4, 5, 22, 31, 45, 35, 26, 78];
-let squareNumbers = numbers.map(x => {
-    return x * x;
-});
+// Traditional function
+function display() {
+    console.log("Display 1");
+}
+display();
 
-console.log(squareNumbers);
+// Arroow function
+const display2 = () => {
+    console.log("Display 2");
+}
+display2();
 
-let arr = numbers.filter(x => {
-    return x >= 10;
-});
-console.log(arr);
+const display3 = () => console.log("Display 3");
+display3();
 
+// Traditional Function
+function message1() {
+    return "Message 1";
+}
+console.log(message1());
+
+const message2 = () => "Message 2";
+console.log(message2());
+
+// Parameter Passing
+function add1(x, y) {
+    return x + y;
+}
+console.log(add1(5, 6));
+
+const add2 = (x, y) => x + y;
+console.log(add2(4, 5));
