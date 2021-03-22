742884a5b094ab89a58b4e6966a17e8c69cb9551
Dipu <dipukumarmohanto1@gmail.com>
2021-03-22 12:28:00 +0600

    find() & findIndex()

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
-// for and forEach
+// find(callback, value) return the value of the first element that passes certain condition
+// findIndex(callback, value) return the index of the first element that passes certain condition
 
-let arr = [10, 20, 30, 40, 50];
-let sqarr = [];
-arr.forEach(function(x) {
-    console.log(x);
-    sqarr.push(x * x);
+let numbers = [5, 55, 14, 5, 78];
+
+let firstEvenNumber = numbers.find((x, index, arr) => {
+    return x % 11 === 0;
 });
+console.log(firstEvenNumber);
 
-console.log(sqarr);
+let firstEvenNumberIndex = numbers.findIndex(x => x % 3 === 0);
+console.log(`first even number index = ${firstEvenNumberIndex}`);
 
-// update array
-let numbers = [1, 2, 3, 4, 5];
-console.log(numbers);
-numbers.forEach(function(x, index, arr) {
-    arr[index] = x + 10;
-});
-console.log(numbers);
+let students = [
+    {
+        id: 101,
+        gpa: 2.5
+    },
+    {
+        id: 102,
+        gpa: 3.5
+    },
+    {
+        id: 104,
+        gpa: 4.5
+    },
+    {
+        id: 110,
+        gpa: 5.0
+    }
+];
+
+let firstGpaGreaterThan4 = students.find(x => (x.gpa > 5));
+console.log(firstGpaGreaterThan4);
