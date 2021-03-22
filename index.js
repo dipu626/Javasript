ebfec03e2b76ad9cc1c3efad8da0c7be56ca6d66
Dipu <dipukumarmohanto1@gmail.com>
2021-03-22 14:12:53 +0600

    ES6 - map, filter

--- index.js
+++ index.js
@@ -1,42 +1,14 @@
-// Array Destructure
+// forEach, map, filter
 
-let numbers = [1, 2, 3, 4, 5];
-let [num1, num2, ...z] = numbers
-console.log(num1);
-console.log(num2);
-console.log(z);
+let numbers = [2, 3, 4, 5, 22, 31, 45, 35, 26, 78];
+let squareNumbers = numbers.map(x => {
+    return x * x;
+});
 
-// swap variables
-let a = 10;
-let b = 20;
-[a, b] = [b, a]
-console.log(`a=${a} b=${b}`);
+console.log(squareNumbers);
 
-// object destructure
-const studentInfo = {
-    id: 101,
-    fullName: "dipu kumar",
-    gpa: 3.34
-}
-const {id, fullName} = studentInfo;
-console.log(`${id} ${fullName}`);
+let arr = numbers.filter(x => {
+    return x >= 10;
+});
+console.log(arr);
 
-// nested object destructure
-const student = {
-    id: 101,
-    name: "dipu kumar",
-    gpa: 3.34,
-    languages: {
-        native: "bangla",
-        beginner: "english"
-    }
-}
-const {name, languages} = student;
-const {native, beginner} = languages;
-console.log(name + " | " + native + ", " + beginner);
-
-// destructing function parameter
-let studentInformation = ({id, fullName}) => {
-    console.log(`funtion = ${id} ${fullName}`);
-}
-studentInformation(studentInfo);
