815e6368e3edab77762434d6bffa06df95c256cf
Dipu <dipukumarmohanto1@gmail.com>
2021-03-23 11:02:35 +0600

    ES6 - Promise: part-1

--- index.js
+++ index.js
@@ -6,68 +6,48 @@ There are some ways to make it synchronus in ES6. They are
     3. async-await method
 */
 
-// 1. Higher Order & Callback functin example
-/* 
-function square(x) {
-    console.log(`square of ${x} is ${x * x}`);
-}
-console.log(square(6));
-// let y = square;
-// console.log(y(7));
-
-function higherOrderFunction(num, callback) {
-    callback(num);
-}
-higherOrderFunction(7, square);
+/*
+2. Promise method
+    2.1 How to create a promise
+    2.2 How to use a promise
+    2.3 How to run multiple promise - all()
+    2.4 race()
+    2.5 Promise chaining
 */
 
-// Using higherOrder & callback function to make JS synchronus
+console.log("welcome");
 
-const taskOne = (callback) => {
-    console.log("This is taskOne");
-    callback();
-}
-const taskTwo = (callback) => {
-    setTimeout(() => {
-        console.log("This is taskTwo");
-        callback();
-    }, 4000);
-}
-const taskThree = (callback) => {
-    console.log("This is taskThree");
-    callback();
-}
-const taskFour = (callback) => {
-    console.log("This is taskFour");
-    callback();
-}
-const taskFive = (callback) => {
-    setTimeout(() => {
-        console.log("This is taskFive");
-        callback();
-    }, 5000);
-}
-const taskSix = (callback) => {
-    console.log("This is taskSix");
-    callback();
-}
-const taskSeven = () => {
-    setTimeout(() => {
-        console.log("This is taskSeven");
-    }, 2000);
-}
-
-// higherOrder & callback function to perform the tasks synchronously
-taskOne(() => {
-    taskTwo(() => {
-        taskThree(() => {
-            taskFour(() => {
-                taskFive(() => {
-                    taskSix(() => {
-                        taskSeven();
-                    })
-                })
-            })
-        })
-    })
+// how to create a promise
+// 3 stage -> pending, resolve, reject
+const promise1 = new Promise((resolve, reject) => {
+    let completedPromise = false;
+    if (completedPromise === true) {
+        resolve("Completed promise 1");
+    }
+    else {
+        reject(new Error("not completed promise 1"));
+    }
 });
+
+const promise2 = new Promise((resolve, reject) => {
+    resolve("completed promise 2");
+});
+
+//console.log(promise1);
+/* promise1.then((message) => {
+    console.log(message);
+}).catch((error) => {
+    console.log(error);
+});
+
+promise2.then((message) => {
+    console.log(message);
+}) */
+
+Promise.all([promise1, promise2]).then((res) => {
+    console.log(res);
+}).catch((error) => {
+    console.log(error);
+})
+
+console.log("bye");
