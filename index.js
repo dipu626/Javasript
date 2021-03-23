860588d487e4423a7b5eaedfa876f9375f59d80d
Dipu <dipukumarmohanto1@gmail.com>
2021-03-23 10:32:07 +0600

    ES6 - higher-order & callback function

--- index.js
+++ index.js
@@ -1,27 +1,73 @@
+/*
+Javascript is a asynchronus programing language. We have to make it synchronus. 
+There are some ways to make it synchronus in ES6. They are
+    1. Higher-order & callback function method
+    2. Promise method
+    3. async-await method
+*/
 
-class Student {
-    constructor(id, name) {
-        this.id = id;
-        this.name = name;
-    }
-    set setId(id) {
-        this.id = id;
-    }
-    set setName(name) {
-        this.name = name;
-    }
-    get getId() {
-        return this.id;
-    }
-    get getName() {
-        return this.name;
-    }
+// 1. Higher Order & Callback functin example
+/* 
+function square(x) {
+    console.log(`square of ${x} is ${x * x}`);
+}
+console.log(square(6));
+// let y = square;
+// console.log(y(7));
+
+function higherOrderFunction(num, callback) {
+    callback(num);
+}
+higherOrderFunction(7, square);
+*/
+
+// Using higherOrder & callback function to make JS synchronus
+
+const taskOne = (callback) => {
+    console.log("This is taskOne");
+    callback();
+}
+const taskTwo = (callback) => {
+    setTimeout(() => {
+        console.log("This is taskTwo");
+        callback();
+    }, 4000);
+}
+const taskThree = (callback) => {
+    console.log("This is taskThree");
+    callback();
+}
+const taskFour = (callback) => {
+    console.log("This is taskFour");
+    callback();
+}
+const taskFive = (callback) => {
+    setTimeout(() => {
+        console.log("This is taskFive");
+        callback();
+    }, 5000);
+}
+const taskSix = (callback) => {
+    console.log("This is taskSix");
+    callback();
+}
+const taskSeven = () => {
+    setTimeout(() => {
+        console.log("This is taskSeven");
+    }, 2000);
 }
 
-let s1 = new Student(101, "dipu");
-let s2 = new Student(102, "akash");
-console.log(s1);
-console.log(s2);
-s2.setName = "bla";
-console.log(s1.getName);
-console.log(s2);
+// higherOrder & callback function to perform the tasks synchronously
+taskOne(() => {
+    taskTwo(() => {
+        taskThree(() => {
+            taskFour(() => {
+                taskFive(() => {
+                    taskSix(() => {
+                        taskSeven();
+                    })
+                })
+            })
+        })
+    })
+});
